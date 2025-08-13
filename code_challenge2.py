be76aa9615cd017c409ac9024748ab934e759ed3
Sylviette <jodeleyclaro13@gmail.com>
2025-08-13 21:40:46 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,5 @@
+n1 = eval(input("Enter amount to deposit"))
+n2 =("1,000,500,200,100,50,20,10,9,8,7,6,5,4,3,2,1")
+
+print("The quotient of" ,n1,"and" ,n2,"is",q)
+print("The floor division of" ,n1,"and" ,n2,"is",n1//n2)
