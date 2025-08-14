9c246920c5ea2e3f998e5fa15ddc4737505f2f5d
Sylviette <jodeleyclaro13@gmail.com>
2025-08-14 10:46:38 +0800

    Delete code_challenge2.py

--- code_challenge2.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input("Enter amount to deposit: "))
-n2 = [1000, 500, 200, 100, 50, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-
-for value in n2:
-    count = n1 // value  # How many of this denomination
-    if count > 0:
-        print(f"{value} x {count}")
-        n1 -= value * count  # Subtract the total of this denomination
