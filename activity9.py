30d83b067d44acf09bf910967c91f77711f741fe
Sylviette <jodeleyclaro13@gmail.com>
2025-08-19 06:44:44 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,11 @@
+x = 3
+y = 5
+
+c = 4
+d = 2
+username = 'jodeley'
+password = 'etopasswordko'
+print((username == 'jodeley')and (password == 'etopasswordko'))
+#print( (x > y) and (c < d) )
+#print( (x > y) or (c < d) )
+#print(not(x > y) or (c <d)))
