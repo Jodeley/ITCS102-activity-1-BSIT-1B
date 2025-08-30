12f69311cfb6ba93ed0b548f682207cf0ac4c94d
Sylviette <jodeleyclaro13@gmail.com>
2025-08-30 22:53:02 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,11 @@
+username = "butterflies_13"
+password = "butterflies123"
+
+uname =input("Type your username here - - > ")
+pword =input("Type your password here - - > ")
+
+if (uname == username) and (pword == password) :
+	print("ACCESS GRANTED")
+
+else:
+	print("ACCESS DENIED")
