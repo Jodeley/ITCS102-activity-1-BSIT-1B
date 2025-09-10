dd7eb2517cc721701f8a91fb47d35ca7c8f545bb
Sylviette <jodeleyclaro13@gmail.com>
2025-09-10 20:06:03 +0800

    Create Code_challenge4.py

---
+++ Code_challenge4.py
@@ -0,0 +1,109 @@
+print("Welcome to the Manga Recommender!")
+print("Answer a few questions to find your next read!")
+genre = input("What genre do you like? (romance/action/ slice of life/horror/sci-fi) ")
+duration = input("How long should the manga be? (short/medium/long) ")
+year = input("What year? (2000s/2010s) ")
+
+if genre == "romance": 
+     if duration == "short":
+       if year == "2000s":
+         print("I recommend Special A by Maki Minami")
+     if duration == "medium":
+       if year == "2000s":
+         print("Kimi ni Todoke by Karuho Shiina")
+     if duration == "long":
+       if year == "2000s":
+         print("Honey Lemon Soda by Mayu Murata")
+
+     if duration == "short":
+       if year == "2010s":
+         print("I recommend Tsurezure Children by Toshiya Wakabayashi")
+     if duration == "medium":
+       if year == "2010s":
+         print("Kimi ni Todoke by Karuho Shiina")
+     if duration == "long":
+       if year == "2010s":
+         print("I recommend Nisekoi by Naoshi Komi")
+# action
+if genre == "action": 
+     if duration == "short":
+       if year == "2000s":
+         print("I recommend Claymore  by Norihiro Yagi")
+     if duration == "medium":
+       if year == "2000s":
+         print("I recommend Code Geass by Goro Taniguchi")
+     if duration == "long":
+       if year == "2000s":
+         print("I recomend Naruto by Masashi Kishimoto")
+     if duration == "short":
+       if year == "2010s":
+         print("I recommend All you need is Kill by Takishi Obata")
+     if duration == "medium":
+       if year == "2010s":
+         print("We don't have recommendation for that right now, sorry")
+     if duration == "long":
+       if year == "2010s":
+         print("I recommend Akami ga Kill by Tetsuya Tashiro")
+# Slice of Life
+if genre == "Slice of life": 
+     if duration == "short":
+       if year == "2000s":
+         print("I ecommend Tori Koro by Hai Ran ")
+     if duration == "medium":
+       if year == "2000s":
+         print("I recommend Naisho No Tsubomi by Yu Yabuchi")
+     if duration == "long":
+       if year == "2000s":
+         print("I recomend And yet the Town Moves by Masakazu Ishiguro")
+     if duration == "short":
+       if year == "2010s":
+         print("I recommend Satoko and Nada by Yupechika")
+     if duration == "medium":
+       if year == "2010s":
+         print("I recommend Silver Spoon by Hiromu Arakawa")
+     if duration == "long":
+       if year == "2010s":
+         print("I don't have enough information for that, sorry")
+# Horror
+if genre == "horror": 
+     if duration == "short":
+       if year == "2000s":
+         print("I recommend Fuan no Tane by Masaaki Nakayama")
+     if duration == "medium":
+       if year == "2000s":
+         print("I recommend I am a Hero by Kengo Hanazawa")
+     if duration == "long":
+       if year == "2000s":
+         print("I recomend Bokurano by Mohiro Kitoh")
+     if duration == "short":
+       if year == "2010s":
+         print("I recommend Hideout by Masasumi Kakizaki")
+     if duration == "medium":
+       if year == "2010s":
+         print("I recommend happiness by Shuzo Oshimi")
+     if duration == "long":
+       if year == "2010s":
+         print("I recommend Tokyo Ghoul by Sui Ishida")
+# Sci-fi
+if genre == "sci-fi": 
+     if duration == "short":
+       if year == "2000s":
+         print("I recommend Narutaru by Mohiro Kitoh")
+     if duration == "medium":
+       if year == "2000s":
+         print("I recommend Blue Heaven by Tsutomu Takahashi")
+     if duration == "long":
+       if year == "2000s":
+         print("I recomend Bokurano by Mohiro Kitoh")
+     if duration == "short":
+       if year == "2010s":
+         print("I recommend I am Space Dandy by Masafumi Harada")
+     if duration == "medium":
+       if year == "2010s":
+         print("I recommend Inuyashiki by Hiroya Oku")
+     if duration == "long":
+       if year == "2010s":
+         print("I recommend World Trigger by Daisuke Ashihara")
+     
+
+     
