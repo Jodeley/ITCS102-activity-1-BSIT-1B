41cfa1441bf0173a4c8a94dcbd2c754c12f830cd
Sylviette <jodeleyclaro13@gmail.com>
2025-09-16 23:07:45 +0800

    Update code_challenge5.py

--- code_challenge5.py
+++ code_challenge5.py
@@ -1,10 +1,10 @@
 name = input("What is your name?---> ").title()
 print("Hello", name, "Welcome to Factorial Calculator!")
-# let the user input number 
 
 fctrl = 1
 nmbr = eval(input("\nEnter the number ---> "))
 for i in range(nmbr, 0, -1):
     fctrl *= i
+
 
 print("The factorial number of", nmbr, "is", fctrl)
