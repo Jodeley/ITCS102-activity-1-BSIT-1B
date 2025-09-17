bbbec454597549eb987150ffd46b1cc2e707d118
Sylviette <jodeleyclaro13@gmail.com>
2025-09-17 22:08:37 +0800

    Add files via upload

---
+++ code_challenge7.py
@@ -0,0 +1,8 @@
+print("Welcome to the Countdown Timer Simulator!")
+print("\n -----------------------------------\n")
+# descending loop
+nmbr = eval(input("Enter a number: "))
+print("Countdown started:")
+for x in range(nmbr, 0, -1):
+  print(x)
+print("Lift off!")
