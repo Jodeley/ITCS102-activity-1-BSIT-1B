2a51c64d48dfe530b96ed1b90cd83649ca006298
Sylviette <jodeleyclaro13@gmail.com>
2025-09-24 11:22:58 +0800

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,6 @@
+# String Formatting
+
+fname = input("Enter your first name ").title()
+mname = input("Enter your middle initial ").title()
+lname = input("Enter your last name ").title()
+print(f"Your full name is {fname} {mname} {lname}")
