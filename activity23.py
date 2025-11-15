5ede8fd0fe241541568a441078407490122617da
Sylviette <jodeleyclaro13@gmail.com>
2025-11-15 12:58:57 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,7 @@
+#                    0                1                     2                        3            4
+fav_anime = ['Darling in Franx', 'Your Name', 'I Want to eat your Pancreas', 'Silent Witch', 'Once Piece']
+len(fav_anime)
+fav_anime.append('Gintama')
+fav_anime.pop()
+fav_anime.sort()
+print(fav_anime)
