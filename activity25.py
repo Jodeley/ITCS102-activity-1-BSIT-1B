5fd7e27b50b45c11a1c9e3b45d87b50d9e01a987
Sylviette <jodeleyclaro13@gmail.com>
2025-11-24 22:36:52 +0800

    Add files via upload

---
+++ activity25.py
@@ -0,0 +1,12 @@
+# Dictionary for lutong bahay
+ulam = ['langka', 'adobong pusit', 'laing', 'fried chicken', 'bikang']
+
+# index
+print(ulam[0])
+
+# {Keys: VALUES, KEYS:VALUES}
+ulam_Dictionary = {'masarap':'langka','mahalang':'adobong pusit','di makati':'laing','malutong':'fried chicken','ok lang':'bikang'}
+ulam_Dictionary['masarap naman'] = 'adobong manok' 
+ulam_Dictionary.pop('ok lang')
+print(ulam_Dictionary)
+
